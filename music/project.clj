d79118cf49b8493f9ac21869d4e6614a2f4f1bfe
Ian Homer <ian.homer@gmail.com>
2016-04-05 11:15:49 +0100

    Create lein project

---
+++ music/project.clj
@@ -0,0 +1,9 @@
+(defproject music "0.1.0-SNAPSHOT"
+  :description "My Music"
+  :url "https://github.com/ianhomer"
+  :license {:name "Apache 2.0 License"
+            :url "http://www.apache.org/licenses/"}
+  :dependencies [
+  	[org.clojure/clojure "1.8.0"]
+	[overtone "0.10.1"]
+  ])
